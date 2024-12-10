42f085bfe976fefdee5e5baa6aab1ae001e45a8d
Takamasa-Fukase <ultrafukase@gmail.com>
2024-12-11 08:24:17 +0900

    FW_A画面の呼び出しを追加

---
+++ Sample_Use_Framework_2/UIView+Constraint.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Constraint.swift
+//  Sample_Use_Framework
+//
+//  Created by ウルトラ深瀬 on 29/10/24.
+//
+
+import UIKit
+
+extension UIView {
+    func addConstraints(for childView: UIView, insets: UIEdgeInsets = .zero) {
+        childView.translatesAutoresizingMaskIntoConstraints = false
+        
+        topAnchor.constraint(equalTo: childView.topAnchor, constant: insets.top).isActive = true
+        bottomAnchor.constraint(equalTo: childView.bottomAnchor, constant: insets.bottom).isActive = true
+        leadingAnchor.constraint(equalTo: childView.leadingAnchor, constant: insets.left).isActive = true
+        trailingAnchor.constraint(equalTo: childView.trailingAnchor, constant: insets.right).isActive = true
+    }
+}
--- Sample_Use_Framework_2/ViewController.swift
+++ Sample_Use_Framework_2/ViewController.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import Sample_Framework_A
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func openFrameworkA_VCButtonTapped(_ sender: Any) {
+        let fwA = SampleFrameworkA()
+        fwA.presentSampleA_VC(on: self)
+    }
 }
-
