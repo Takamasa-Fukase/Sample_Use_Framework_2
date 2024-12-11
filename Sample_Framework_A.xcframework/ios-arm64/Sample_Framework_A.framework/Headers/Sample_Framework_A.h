5844b30a956ddf18b7170986477bd8b4a768c712
Takamasa-Fukase <ultrafukase@gmail.com>
2024-12-11 14:46:25 +0900

    FW_A.xcframeworkを.xcodeprojと同じ階層にコピー。レポジトリに含めて管理するように変更

---
+++ Sample_Framework_A.xcframework/ios-arm64/Sample_Framework_A.framework/Headers/Sample_Framework_A.h
@@ -0,0 +1,18 @@
+//
+//  Sample_Framework_A.h
+//  Sample_Framework_A
+//
+//  Created by ウルトラ深瀬 on 10/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sample_Framework_A.
+FOUNDATION_EXPORT double Sample_Framework_AVersionNumber;
+
+//! Project version string for Sample_Framework_A.
+FOUNDATION_EXPORT const unsigned char Sample_Framework_AVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sample_Framework_A/PublicHeader.h>
+
+
